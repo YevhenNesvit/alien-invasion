66b43c94a324a6b02c3197e8eba02623cff525f5
Yevhen Nesvit <yevhen.nesvit@gmail.com>
2023-11-14 12:02:33 +0200

    initial commit

---
+++ alien_invasion.py
@@ -0,0 +1,26 @@
+import sys
+import pygame
+
+def run_game():
+    # Инициализирует игру и создает объект экрана.
+    pygame.init()
+    screen = pygame.display.set_mode((1200, 800))
+    pygame.display.set_caption("Alien Invasion")
+    
+    # Назначение цвета фона.
+    bg_color = (230, 230, 230)
+    
+    # Запуск основного цикла игры.
+    while True:
+    # Отслеживание событий клавиатуры и мыши.
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+        # При каждом проходе цикла перерисовывается экран.
+        screen.fill(bg_color)
+        
+        # Отображение последнего прорисованного экрана.
+        pygame.display.flip()
+
+run_game()
