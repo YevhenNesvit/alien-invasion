a9f25094911be284a77fa2f6eb7178911b79dc51
Yevhen Nesvit <yevhen.nesvit@gmail.com>
2023-12-07 16:59:13 +0200

    add fleet

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,7 +2,6 @@ import pygame
 from pygame.sprite import Group
 from settings import Settings
 from ship import Ship
-from alien import Alien
 import game_functions as gf
 
 def run_game():
@@ -11,18 +10,18 @@ def run_game():
     ai_settings = Settings()
     screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
-    # Создание корабля
+    # Создание корабля, группы пуль и группы пришельцев.
     ship = Ship(ai_settings, screen)
-    # Создание группы для хранения пуль.
     bullets = Group()
-    # Создание пришельца.
-    alien = Alien(ai_settings, screen)
+    aliens = Group()
+    # Создание флота пришельцев.
+    gf.create_fleet(ai_settings, screen, aliens)
     
     # Запуск основного цикла игры.
     while True:
         gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
         gf.update_bullets(bullets)
-        gf.update_screen(ai_settings, screen, ship, alien, bullets)
+        gf.update_screen(ai_settings, screen, ship, aliens, bullets)
 
 run_game()
