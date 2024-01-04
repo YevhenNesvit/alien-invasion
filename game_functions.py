dfc61cf9b557a64c7e09d05eb105e8f0096d76aa
Yevhen Nesvit <yevhen.nesvit@gmail.com>
2024-01-04 20:08:35 +0200

    add bottom colision

--- game_functions.py
+++ game_functions.py
@@ -146,16 +146,19 @@ def ship_hit(ai_settings, stats, screen, ship, aliens, bullets):
     """
     Обрабатывает столкновение корабля с пришельцем.
     """
-    # Уменьшение ships_left.
-    stats.ships_left -= 1
-    # Очистка списков пришельцев и пуль.
-    aliens.empty()
-    bullets.empty()
-    # Создание нового флота и размещение корабля в центре.
-    create_fleet(ai_settings, screen, ship, aliens)
-    ship.center_ship()
-    # Пауза.
-    sleep(0.5)
+    if stats.ships_left > 0:
+        # Уменьшение ships_left.
+        stats.ships_left -= 1
+        # Очистка списков пришельцев и пуль.
+        aliens.empty()
+        bullets.empty()
+        # Создание нового флота и размещение корабля в центре.
+        create_fleet(ai_settings, screen, ship, aliens)
+        ship.center_ship()
+        # Пауза.
+        sleep(0.5)
+    else:
+        stats.game_active = False
 
 def update_aliens(ai_settings, stats, screen, ship, aliens, bullets):
     """
@@ -166,3 +169,16 @@ def update_aliens(ai_settings, stats, screen, ship, aliens, bullets):
     # Проверка коллизий "пришелец-корабль".
     if pygame.sprite.spritecollideany(ship, aliens):
         ship_hit(ai_settings, stats, screen, ship, aliens, bullets)
+    # Проверка пришельцев, добравшихся до нижнего края экрана.
+    check_aliens_bottom(ai_settings, stats, screen, ship, aliens, bullets)
+
+def check_aliens_bottom(ai_settings, stats, screen, ship, aliens, bullets):
+    """
+    Проверяет, добрались ли пришельцы до нижнего края экрана.
+    """
+    screen_rect = screen.get_rect()
+    for alien in aliens.sprites():
+        if alien.rect.bottom >= screen_rect.bottom:
+            # Происходит то же, что при столкновении с кораблем.
+            ship_hit(ai_settings, stats, screen, ship, aliens, bullets)
+            break
