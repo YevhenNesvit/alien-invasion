0a1b4efc7dfbe80163238a8992d597c8a99e7bc3
Yevhen Nesvit <yevhen.nesvit@gmail.com>
2024-01-13 20:47:58 +0200

    add reset function

--- game_functions.py
+++ game_functions.py
@@ -4,7 +4,7 @@ import pygame
 from bullet import Bullet
 from alien import Alien
 
-def check_events(ai_settings, screen, stats, play_button, ship, bullets):
+def check_events(ai_settings, screen, stats, play_button, ship, aliens, bullets):
     """
     Обрабатывает нажатия клавиш и события мыши.
     """
@@ -13,18 +13,26 @@ def check_events(ai_settings, screen, stats, play_button, ship, bullets):
             sys.exit()
         elif event.type == pygame.MOUSEBUTTONDOWN:
             mouse_x, mouse_y = pygame.mouse.get_pos()
-            check_play_button(stats, play_button, mouse_x, mouse_y)
+            check_play_button(ai_settings, screen, stats, play_button, ship, aliens, bullets, mouse_x, mouse_y)
         elif event.type == pygame.KEYDOWN:
             check_keydown_events(event, ai_settings, screen, ship, bullets)
         elif event.type == pygame.KEYUP:
             check_keyup_events(event, ship)
 
-def check_play_button(stats, play_button, mouse_x, mouse_y):
+def check_play_button(ai_settings, screen, stats, play_button, ship, aliens, bullets, mouse_x, mouse_y):
     """
     Запускает новую игру при нажатии кнопки Play.
     """
     if play_button.rect.collidepoint(mouse_x, mouse_y):
+        # Сброс игровой статистики.
+        stats.reset_stats()
         stats.game_active = True
+        # Очистка списков пришельцев и пуль.
+    aliens.empty()
+    bullets.empty()
+    # Создание нового флота и размещение корабля в центре.
+    create_fleet(ai_settings, screen, ship, aliens)
+    ship.center_ship()
 
 def update_screen(ai_settings, screen, stats, ship, aliens, bullets, play_button):
     """
